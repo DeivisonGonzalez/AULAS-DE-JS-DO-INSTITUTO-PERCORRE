5d3445470781b03f8ccc684872f1ed1e87aae05a
DeivisonGonzalez <deivisoncastro40@gmail.com>
2026-05-21 09:56:42 -0300

    Add files via upload

---
+++ JS - Deivi/JS - Deivi/EXERCICIOJS/conferir_idade.js
@@ -0,0 +1,14 @@
+idade = 18
+
+if (idade < 18) {
+    console.log("Usuário menor de idade");
+}
+
+else if (idade >= 18 && idade <= 60) {
+    console.log('Usuário adulto');
+}
+
+else {
+    console.log("Usuário idoso");
+    
+}
